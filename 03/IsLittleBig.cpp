6be789ca640ff508ff2831ec2eccb10d9ba7fc29
laptop-msi <wlgns7922@gmail.com>
2026-09-13 00:17:03 +0900

    과제1: 호스트의 리틀엔디안 빅엔디안 정렬 여부 판별 함수

---
+++ 03/IsLittleBig.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS // 구형 C 함수 사용 시 경고 끄기
+#define _WINSOCK_DEPRECATED_NO_WARNINGS // 구형 소켓 API 사용 시 경고 끄기
+
+#include <winsock2.h> // 윈속2 메인 헤더
+#include <ws2tcpip.h> // 윈속2 확장 헤더
+
+#include <tchar.h> // _T(), ...
+#include <stdio.h> // printf(), ...
+#include <stdlib.h> // exit(), ...
+#include <string.h> // strncpy(), ...
+
+#pragma comment(lib, "ws2_32") // ws2_32.lib 링크
+
+// 소켓 함수 오류 출력 후 종료
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+bool IsLittleEndian()
+{
+	u_short temp = 0x1234;
+	if (temp != htons(temp)) {
+		printf("The Host uses Little-Endian order\n");
+		return true;
+	}
+	return false;
+}
+
+bool IsBigEndian()
+{
+	u_short temp = 0x1234;
+	if (temp == htons(temp)) {
+		printf("The Host uses Big-Endian order\n");
+		return true;
+	}
+	return false;
+}
+
+int main(int argc, char* argv[])	
+{
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	IsLittleEndian();
+	IsBigEndian();
+
+	if (WSACleanup() == SOCKET_ERROR) err_quit("WSACleanUp()");
+
+	return 0;
+}
